b8c3f56b60d7536509b3168b9a26a87992a9ebe1
siddiquesakib <dmsakib1122@gmail.com>
2026-01-04 19:10:41 +0600

    fixed data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port =  3000;
+const port = 3000;
 require("dotenv").config();
 
 app.use(cors());
@@ -24,6 +24,130 @@ async function run() {
     const db = client.db("AdoptiPaws-server");
     const listingsColl = db.collection("listings");
     const orderColl = db.collection("OrderColl");
+    const usersColl = db.collection("users");
+    const donationsColl = db.collection("donations");
+    const adoptionRequestsColl = db.collection("adoptionRequests");
+
+    // Users Routes
+    app.get("/users", async (req, res) => {
+      const result = await usersColl.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log("Fetching user by email:", email);
+      const query = { email: email };
+      const result = await usersColl.findOne(query);
+      console.log("User found:", result ? "Yes" : "No");
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("Attempting to create user:", user.email);
+      const query = { email: user.email };
+      const existingUser = await usersColl.findOne(query);
+
+      if (existingUser) {
+        console.log("User already exists:", user.email);
+        return res.send({ message: "User already exists", insertedId: null });
+      }
+
+      const result = await usersColl.insertOne(user);
+      console.log("User created successfully:", user.email);
+      res.send(result);
+    });
+
+    app.patch("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const { role } = req.body;
+      const objectid = new ObjectId(id);
+
+      const update = {
+        $set: { role: role },
+      };
+
+      const result = await usersColl.updateOne({ _id: objectid }, update);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectid = new ObjectId(id);
+      const result = await usersColl.deleteOne({ _id: objectid });
+      res.send(result);
+    });
+
+    // Donations Routes
+    app.get("/donations", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const result = await donationsColl.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/donations", async (req, res) => {
+      const data = req.body;
+      const result = await donationsColl.insertOne(data);
+      res.send(result);
+    });
+
+    app.patch("/donations/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectid = new ObjectId(id);
+      const updateData = req.body;
+
+      const update = {
+        $set: updateData,
+      };
+
+      const result = await donationsColl.updateOne({ _id: objectid }, update);
+      res.send(result);
+    });
+
+    app.delete("/donations/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectid = new ObjectId(id);
+      const result = await donationsColl.deleteOne({ _id: objectid });
+      res.send(result);
+    });
+
+    // Adoption Requests Routes
+    app.get("/adoption-requests", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.ownerEmail = email; // Owner's email who listed the pet
+      }
+      const result = await adoptionRequestsColl.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/adoption-requests", async (req, res) => {
+      const data = req.body;
+      const result = await adoptionRequestsColl.insertOne(data);
+      res.send(result);
+    });
+
+    app.patch("/adoption-requests/:id", async (req, res) => {
+      const { id } = req.params;
+      const { status } = req.body;
+      const objectid = new ObjectId(id);
+
+      const update = {
+        $set: { status: status },
+      };
+
+      const result = await adoptionRequestsColl.updateOne(
+        { _id: objectid },
+        update
+      );
+      res.send(result);
+    });
 
     app.get("/latest-pets", async (req, res) => {
       const result = await listingsColl
